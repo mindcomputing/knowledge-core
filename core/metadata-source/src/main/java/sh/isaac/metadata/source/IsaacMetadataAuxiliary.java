efd3dbba9268681ea0db81f6e3dc694d471b7326
Keith E. Campbell <campbell@informatics.com>
2018-10-14 21:32:21 -0700

    Added stamp coordinate state, path, and time editing.

--- core/metadata-source/src/main/java/sh/isaac/metadata/source/IsaacMetadataAuxiliary.java
+++ core/metadata-source/src/main/java/sh/isaac/metadata/source/IsaacMetadataAuxiliary.java
@@ -926,6 +926,7 @@ public class IsaacMetadataAuxiliary extends IsaacTaxonomy {
             createConcept("Query clause parameters");
             pushParent(current());
                createConcept("Query string");
+               createConcept("Let item key");
                popParent();
             popParent(); 
          popParent(); // ISAAC root should still be parent on stack...
