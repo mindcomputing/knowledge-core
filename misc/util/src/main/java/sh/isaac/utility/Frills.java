logicahealth/komet

Initial support for non-defining taxonomies.

misc/util/src/main/java/sh/isaac/utility/Frills.java
@@ -73,7 +73,6 @@ import sh.isaac.api.Get;
 import sh.isaac.api.IsaacCache;
 import sh.isaac.api.LookupService;
 import sh.isaac.api.Status;
-import sh.isaac.api.TaxonomySnapshotService;
 import sh.isaac.api.bootstrap.TermAux;
 import sh.isaac.api.chronicle.Chronology;
 import sh.isaac.api.chronicle.LatestVersion;
@@ -152,6 +151,7 @@ import sh.isaac.model.semantic.version.DynamicImpl;
 import sh.isaac.model.semantic.version.LogicGraphVersionImpl;
 import sh.isaac.model.semantic.version.LongVersionImpl;
 import sh.isaac.model.semantic.version.StringVersionImpl;
+import sh.isaac.api.TaxonomySnapshot;
 
 
 /**
@@ -1032,7 +1032,7 @@ public class Frills
     */
    public static Set<Integer> getAllChildrenOfConcept(int conceptNid, boolean recursive, boolean leafOnly, StampCoordinate stamp) {
       
-      TaxonomySnapshotService tss = Get.taxonomyService().getSnapshot(
+      TaxonomySnapshot tss = Get.taxonomyService().getSnapshot(
             new ManifoldCoordinateImpl((stamp == null ? Get.configurationService().getUserConfiguration(Optional.empty()).getStampCoordinate() : stamp),
                   LanguageCoordinates.getUsEnglishLanguageFullySpecifiedNameCoordinate()));
       
@@ -1047,7 +1047,7 @@ public class Frills
     * Recursively get Is a children of a concept.  May inadvertently return the requested starting sequence when leafOnly is true, and 
     * there are no children.
     */
-   private static Set<Integer> getAllChildrenOfConcept(Set<Integer> handledConceptNids, int conceptNid, boolean recursive, boolean leafOnly, TaxonomySnapshotService tss) {
+   private static Set<Integer> getAllChildrenOfConcept(Set<Integer> handledConceptNids, int conceptNid, boolean recursive, boolean leafOnly, TaxonomySnapshot tss) {
       Set<Integer> results = new HashSet<>();
 
       // This both prevents infinite recursion and avoids processing or returning of duplicates
@@ -2064,7 +2064,7 @@ public class Frills
       HashSet<Integer> modules = new HashSet<>();
       HashSet<Integer> terminologyTypes = new HashSet<>();
       
-      TaxonomySnapshotService tss = Get.taxonomyService().getStatedLatestSnapshot(
+      TaxonomySnapshot tss = Get.taxonomyService().getStatedLatestSnapshot(
             (stamp == null ? StampCoordinates.getDevelopmentLatest().getStampPosition().getStampPathNid() : stamp.getStampPosition().getStampPathNid()),
             (stamp == null ? NidSet.EMPTY : stamp.getModuleNids()),
             (stamp == null ? Status.ACTIVE_ONLY_SET : stamp.getAllowedStates()));

provider/query/src/main/java/sh/isaac/provider/query/lucene/indexers/DescriptionIndexer.java
@@ -41,7 +41,6 @@ import com.github.benmanes.caffeine.cache.Caffeine;
 import sh.isaac.api.Get;
 import sh.isaac.api.LookupService;
 import sh.isaac.api.Status;
-import sh.isaac.api.TaxonomySnapshotService;
 import sh.isaac.api.bootstrap.TermAux;
 import sh.isaac.api.chronicle.Chronology;
 import sh.isaac.api.chronicle.LatestVersion;
@@ -66,6 +65,7 @@ import sh.isaac.model.coordinate.StampCoordinateImpl;
 import sh.isaac.model.coordinate.StampPositionImpl;
 import sh.isaac.provider.query.lucene.LuceneIndexer;
 import sh.isaac.provider.query.lucene.PerFieldAnalyzer;
+import sh.isaac.api.TaxonomySnapshot;
 
 /**
  * Lucene Manager which specializes in indexing descriptions.
@@ -151,7 +151,7 @@ public class DescriptionIndexer extends LuceneIndexer
          {
             isMetadata = isMetadataCache.get(key, pathAndRefComp -> {
             //cache doesn't have the answer, needs to calculate.  We construct a snapshot of latest time, the path, and any module, active only.
-            TaxonomySnapshotService tss = Get.taxonomyService().getSnapshot(new ManifoldCoordinateImpl(
+            TaxonomySnapshot tss = Get.taxonomyService().getSnapshot(new ManifoldCoordinateImpl(
                   new StampCoordinateImpl(StampPrecedence.PATH, new StampPositionImpl(Long.MAX_VALUE, pathNid), NidSet.EMPTY, new int[0], Status.ACTIVE_ONLY_SET), null));
             return tss.isKindOf(semanticChronology.getReferencedComponentNid(), TermAux.SOLOR_METADATA.getNid());
             }).booleanValue();

mindcomputing/knowledge-core

Conflicts:
	core/api/src/main/java/sh/isaac/api/TaxonomyService.java
	core/api/src/main/java/sh/isaac/api/TaxonomySnapshotService.java
	misc/util/src/main/java/sh/isaac/utility/Frills.java
	provider/chronology/src/main/java/sh/isaac/provider/datastore/taxonomy/TaxonomyProvider.java
	provider/query/src/main/java/sh/isaac/provider/query/lucene/indexers/DescriptionIndexer.java

misc/util/src/main/java/sh/isaac/utility/Frills.java
@@ -74,7 +74,6 @@ import sh.isaac.api.Get;
 import sh.isaac.api.IsaacCache;
 import sh.isaac.api.LookupService;
 import sh.isaac.api.Status;
-import sh.isaac.api.TaxonomySnapshotService;
 import sh.isaac.api.bootstrap.TermAux;
 import sh.isaac.api.chronicle.Chronology;
 import sh.isaac.api.chronicle.LatestVersion;
@@ -155,6 +154,7 @@ import sh.isaac.model.semantic.version.DynamicImpl;
 import sh.isaac.model.semantic.version.LogicGraphVersionImpl;
 import sh.isaac.model.semantic.version.LongVersionImpl;
 import sh.isaac.model.semantic.version.StringVersionImpl;
+import sh.isaac.api.TaxonomySnapshot;
 
 /**
  * The Class Frills.
@@ -1106,7 +1106,7 @@ public class Frills
     */
    public static Set<Integer> getAllChildrenOfConcept(int conceptNid, boolean recursive, boolean leafOnly, StampCoordinate stamp) {
       
-      TaxonomySnapshotService tss = Get.taxonomyService().getSnapshotNoTree(
+        TaxonomySnapshot tss = Get.taxonomyService().getSnapshotNoTree(
             new ManifoldCoordinateImpl((stamp == null ? Get.configurationService().getUserConfiguration(Optional.empty()).getStampCoordinate() : stamp),
                   LanguageCoordinates.getUsEnglishLanguageFullySpecifiedNameCoordinate()));
       
@@ -1121,7 +1121,7 @@ public class Frills
     * Recursively get Is a children of a concept.  May inadvertently return the requested starting sequence when leafOnly is true, and 
     * there are no children.
     */
-   private static Set<Integer> getAllChildrenOfConcept(Set<Integer> handledConceptNids, int conceptNid, boolean recursive, boolean leafOnly, TaxonomySnapshotService tss) {
+   private static Set<Integer> getAllChildrenOfConcept(Set<Integer> handledConceptNids, int conceptNid, boolean recursive, boolean leafOnly, TaxonomySnapshot tss) {
       Set<Integer> results = new HashSet<>();
 
       // This both prevents infinite recursion and avoids processing or returning of duplicates
@@ -2138,7 +2138,7 @@ public class Frills
       HashSet<Integer> modules = new HashSet<>();
       HashSet<Integer> terminologyTypes = new HashSet<>();
       
-      TaxonomySnapshotService tss = Get.taxonomyService().getStatedLatestSnapshot(
+      TaxonomySnapshot tss = Get.taxonomyService().getStatedLatestSnapshot(
             (stamp == null ? StampCoordinates.getDevelopmentLatest().getStampPosition().getStampPathNid() : stamp.getStampPosition().getStampPathNid()),
             (stamp == null ? NidSet.EMPTY : stamp.getModuleNids()),
             (stamp == null ? Status.ACTIVE_ONLY_SET : stamp.getAllowedStates()), false);

provider/query/src/main/java/sh/isaac/provider/query/lucene/indexers/DescriptionIndexer.java
@@ -41,7 +41,6 @@ import com.github.benmanes.caffeine.cache.Caffeine;
 import sh.isaac.api.Get;
 import sh.isaac.api.LookupService;
 import sh.isaac.api.Status;
-import sh.isaac.api.TaxonomySnapshotService;
 import sh.isaac.api.bootstrap.TermAux;
 import sh.isaac.api.chronicle.Chronology;
 import sh.isaac.api.chronicle.LatestVersion;
@@ -66,6 +65,7 @@ import sh.isaac.model.coordinate.StampCoordinateImpl;
 import sh.isaac.model.coordinate.StampPositionImpl;
 import sh.isaac.provider.query.lucene.LuceneIndexer;
 import sh.isaac.provider.query.lucene.PerFieldAnalyzer;
+import sh.isaac.api.TaxonomySnapshot;
 
 /**
  * Lucene Manager which specializes in indexing descriptions.
@@ -154,7 +154,7 @@ public class DescriptionIndexer extends LuceneIndexer
             try 
             {
                //cache doesn't have the answer, needs to calculate.  We construct a snapshot of latest time, the path, and any module, active only.
-               TaxonomySnapshotService tss = Get.taxonomyService().getSnapshotNoTree(new ManifoldCoordinateImpl(
+                TaxonomySnapshot tss = Get.taxonomyService().getSnapshotNoTree(new ManifoldCoordinateImpl(
                      new StampCoordinateImpl(StampPrecedence.PATH, new StampPositionImpl(Long.MAX_VALUE, pathNid), NidSet.EMPTY, new int[0], Status.ACTIVE_ONLY_SET), null));
                return tss.isKindOf(semanticChronology.getReferencedComponentNid(), TermAux.SOLOR_METADATA.getNid());
             }